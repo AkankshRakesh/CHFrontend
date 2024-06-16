3107bb0426970277989b18043962803971daeb01
AkankshRakesh <akankshrakesh@gmail.com>
2024-06-16 16:12:59 +0530

    changed

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const mongoDB = require('./db');
+const createUserRoutes = require('./Routes/CreateUser');
+const displayDataRoutes = require('./Routes/DisplayData');
+const careerDataRoutes = require('./Routes/CareerData');
+
+const app = express();
 const port = process.env.PORT || 5000;
-const mongoDB = require("./db")
-mongoDB()
 
-app.use((req,res,next) => {
-  res.setHeader("Access-Control-Allow-Origin","*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept" 
-  );
-  next();
-})
+// Connect to MongoDB
+mongoDB();
 
+// Middleware
+app.use(cors()); // Using cors middleware
+app.use(express.json()); // For parsing application/json
+
+// Routes
+app.use('/api', createUserRoutes);
+app.use('/api', displayDataRoutes);
+app.use('/api', careerDataRoutes);
+
+// Default route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.use(express.json())
-app.use('/api',require("./Routes/CreateUser"));
-app.use('/api',require("./Routes/DisplayData"));
-app.use('/api',require("./Routes/CareerData"));
+// Start server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
