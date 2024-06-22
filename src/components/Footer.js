bdc306ea98cd0a88c6fb86ec57c2dc246cf2aee4
AkankshRakesh <akankshrakesh@gmail.com>
2024-06-22 17:35:29 +0530

    footer changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,6 @@ export default function Footer() {
     <div>
       <div className="container">
   <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <p className="col-md-4 mb-0 text-muted">© 2024 ZobZilla, Inc</p>
 
     <Link to="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
     <img src="https://imgdb.net/storage/uploads/81189a12e8e2f654bc12df3f9e4421e929e8916882cf0d10355828b72110e8f1.png" alt="" width="90" height="70"/>
@@ -14,9 +13,6 @@ export default function Footer() {
 
     <ul className="nav col-md-4 justify-content-end">
       <li className="nav-item"><Link to="/" className="nav-link px-2 text-muted">Home</Link></li>
-      <li className="nav-item"><Link to="/" className="nav-link px-2 text-muted">Features</Link></li>
-      <li className="nav-item"><Link to="/" className="nav-link px-2 text-muted">Pricing</Link></li>
-      <li className="nav-item"><Link to="/" className="nav-link px-2 text-muted">FAQs</Link></li>
       <li className="nav-item"><Link to="/" className="nav-link px-2 text-muted">About</Link></li>
     </ul>
   </footer>
